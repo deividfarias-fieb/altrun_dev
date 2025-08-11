4958bf0b2744545e36375e06f48b22fec280f8f8
deividfarias-fieb <deivid.farias@docente.fieb.edu.br>
2025-08-11 09:37:31 -0300

    Update Egg 0.0.1

--- Assets/Scripts/Mouse_Controller.cs
+++ Assets/Scripts/Mouse_Controller.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.Rendering.Universal;
 
 public class Mouse_Controller : MonoBehaviour
 {
     [SerializeField] private GameObject mouseSprite;
+    [SerializeField] private GameObject lightMouse;
     // Start is called before the first frame update
     void Start()
     {
         mouseSprite.GetComponent<SpriteRenderer>().enabled = false;
+        lightMouse = GameObject.FindGameObjectWithTag("Mouse_Egg");
+        lightMouse.GetComponent<Light2D>().intensity = 0f;
     }
 
     public void OpenEgg()
@@ -18,8 +23,10 @@ public class Mouse_Controller : MonoBehaviour
 
     IEnumerator EnableEasterEgg()
     {
+        lightMouse.GetComponent<Light2D>().intensity = 1.5f;
         mouseSprite.GetComponent<SpriteRenderer>().enabled = true;
         yield return new WaitForSeconds(1.9f);
         mouseSprite.GetComponent<SpriteRenderer>().enabled = false;
+        lightMouse.GetComponent<Light2D>().intensity = 0f;
     }
 }
